563a11f8562142863367022f2213f9ae87f5eb53
anil-mawji <mawjjianil1@gmail.com>
2024-08-30 00:39:55 -0600

    Clean up

--- src/components/sections/Bio.tsx
+++ src/components/sections/Bio.tsx
@@ -13,11 +13,23 @@ const BioSection = () => {
         I'm now diving into ethical hacking as I work towards earning my PJPT from TCM Security.
         <br />
         <br />
-        Currently, I hold the following certifications:
+        I currently hold the following certifications:
         <ul>
-          <li>CompTIA A+</li>
-          <li>CompTIA Security+</li>
-          <li>Microsoft Azure Fundamentals (AZ-900)</li>
+          <li>
+            <a href="https://www.credly.com/badges/e8b164ea-078d-4862-9346-a6ff8fd2547e" target="_blank">
+              CompTIA A+
+            </a>
+          </li>
+          <li>
+            <a href="https://www.credly.com/badges/79231690-ded9-4d5f-be98-7f74f4ebe95c" target="_blank">
+              CompTIA Security+
+            </a>
+          </li>
+          <li>
+            <a href="https://www.credly.com/badges/d63c2487-af0c-4141-a69f-f2b17d503b47" target="_blank">
+              Microsoft Azure Fundamentals (AZ-900)
+            </a>
+          </li>
         </ul>
         <br />
         I'm always open to meeting new people, so feel free to reach out if you want to connect.
