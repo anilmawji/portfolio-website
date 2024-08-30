928f7266e63f2552d9124e1579c1053d55510f25
anil-mawji <mawjjianil1@gmail.com>
2024-08-30 00:21:38 -0600

    Fix bio styling

--- src/components/sections/Bio.tsx
+++ src/components/sections/Bio.tsx
@@ -7,9 +7,19 @@ const BioSection = () => {
       <img src={portrait} />
       <p>
         Hi, I'm Anil, a Computer Science student at the University of Calgary.
+        <br />
+        <br />
         My background includes hands-on experience in both the public and private sectors as a systems administrator and a software developer.
         I'm now diving into ethical hacking as I work towards earning my PJPT from TCM Security.
-        Currently, I hold the CompTIA A+, CompTIA Security+, AZ-900 and SC-900 certifications.
+        <br />
+        <br />
+        Currently, I hold the following certifications:
+        <ul>
+          <li>CompTIA A+</li>
+          <li>CompTIA Security+</li>
+          <li>Microsoft Azure Fundamentals (AZ-900)</li>
+        </ul>
+        <br />
         I'm always open to meeting new people, so feel free to reach out if you want to connect.
         Stay tuned for plenty of cybersecurity-related content!
       </p>
