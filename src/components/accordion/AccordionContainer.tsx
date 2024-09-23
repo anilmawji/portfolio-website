37800a469b00f94a5a0679896dfd03fa0accc6aa
anilmawji <mawjianil1@gmail.com>
2024-09-23 03:22:25 -0600

    Generalize AccordionContainer params

--- src/components/accordion/AccordionContainer.tsx
+++ src/components/accordion/AccordionContainer.tsx
@@ -1,6 +1,6 @@
 import styles from './AccordionContainer.module.css';
 import { useState, useCallback } from 'react';
-import { joinClassNames, ReactCSSVariables, getModifiedReactNode } from '../../utils';
+import { joinClassNames, ReactCSSVariables, ReactNodeOrNodeProvider, getModifiedReactNode } from '../../utils';
 import { Accordion, AccordionData } from './Accordion';
 
 interface Props {
@@ -8,50 +8,49 @@ interface Props {
   className?: string;
   accordionData: AccordionData[];
   gap?: string;
-  children?: React.ReactNode | ((data: AccordionData, index: number) => React.ReactNode);
-  footerChildren?: React.ReactNode | ((data: AccordionData, index: number) => React.ReactNode);
+  children?: ReactNodeOrNodeProvider;
+  footerChildren?: ReactNodeOrNodeProvider;
 }
-  
-  const AccordionContainer = ({ id, className, accordionData, gap = '20px', children, footerChildren }: Props) => {
-    const [openAccordions, setOpenAccordions] = useState<boolean[]>(Array(accordionData.length).fill(false));
 
-    const containerStyle: ReactCSSVariables = {
-      '--accordion-gap': gap
-    };
-
-    const toggleAccordion = useCallback((index: number) => {
-      setOpenAccordions(prev => {
-        const newOpened = [...prev];
-        newOpened[index] = !newOpened[index];
-        return newOpened;
-      });
-    }, []);
-
-    return (
-      <div id={id} className={joinClassNames(styles.container, className)} style={containerStyle}>
-        {accordionData.map((data, i) => {
-          return (
-            <div key={i}>
-              {i !== 0 && (
-                <div
-                  className={joinClassNames(
-                    styles.spacer,
-                    openAccordions[i] || openAccordions[i - 1] ? styles.spacerExpanded : ''
-                  )}
-                />
-              )}
-              <Accordion
-                data={data}
-                toggleCallback={() => toggleAccordion(i)}
-                footerChildren={getModifiedReactNode(footerChildren, data, i)}
-              >
-                {getModifiedReactNode(children, data, i)}
-              </Accordion>
-            </div>
-          );
-        })}
-      </div>
-    );
+const AccordionContainer = ({ id, className, accordionData, gap = '20px', children, footerChildren }: Props) => {
+  const [openAccordions, setOpenAccordions] = useState<boolean[]>(Array(accordionData.length).fill(false));
+  const containerStyle: ReactCSSVariables = {
+    '--accordion-gap': gap
   };
+
+  const toggleAccordion = useCallback((index: number) => {
+    setOpenAccordions(prev => {
+      const newOpened = [...prev];
+      newOpened[index] = !newOpened[index];
+      return newOpened;
+    });
+  }, []);
+
+  return (
+    <div id={id} className={joinClassNames(styles.container, className)} style={containerStyle}>
+      {accordionData.map((data, i) => {
+        return (
+          <div key={i}>
+            {i !== 0 && (
+              <div
+                className={joinClassNames(
+                  styles.spacer,
+                  openAccordions[i] || openAccordions[i - 1] ? styles.spacerExpanded : ''
+                )}
+              />
+            )}
+            <Accordion
+              data={data}
+              toggleCallback={() => toggleAccordion(i)}
+              footerChildren={getModifiedReactNode(footerChildren, data, i)}
+            >
+              {getModifiedReactNode(children, data, i)}
+            </Accordion>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
   
   export default AccordionContainer;
