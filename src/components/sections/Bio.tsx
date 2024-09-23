7e465bd177c3cbadb4c5454405be9b72cc21487d
anilmawji <mawjianil1@gmail.com>
2024-09-23 04:00:49 -0600

    Bullet margins instead of padding

--- src/components/sections/Bio.tsx
+++ src/components/sections/Bio.tsx
@@ -29,21 +29,6 @@ const BioSection = ({ className }: Props) => {
             the <a className={globalStyles.hoverUnderline} href="https://www.canada.ca/en.html" target="_blank">federal government.</a> I'm currently diving into the world of ethical hacking
             as I work towards earning my <a className={globalStyles.hoverUnderline} href="https://certifications.tcm-sec.com/pjpt/" target="_blank">PJPT certification.</a>
             <br /><br />
-            Here are some of the certifications I've picked up so far:
-          </p>
-          <ul>
-            <li>
-              <a className={globalStyles.hoverUnderline} href="https://www.credly.com/badges/79231690-ded9-4d5f-be98-7f74f4ebe95c" target="_blank">CompTIA Security+</a>
-            </li>
-            <li>
-              <a className={globalStyles.hoverUnderline} href="https://www.credly.com/badges/e8b164ea-078d-4862-9346-a6ff8fd2547e" target="_blank">CompTIA A+</a>
-            </li>
-            <li>
-              <a className={globalStyles.hoverUnderline} href="https://www.credly.com/badges/d63c2487-af0c-4141-a69f-f2b17d503b47" target="_blank">Microsoft Azure Fundamentals (AZ-900)</a>
-            </li>
-          </ul>
-          <p>
-            <br />
             When I'm not at my computer, I enjoy hiking, swimming and going out for a bite with friends.
             Feel free to <NavLink className={globalStyles.hoverUnderline} to="/about#contact">reach out</NavLink> if you'd like to chat!
           </p>
