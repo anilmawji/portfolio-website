e1bc4d685348eb9c2abc9c5f950927d640159bf8
anilmawji <mawjianil1@gmail.com>
2024-10-02 22:10:11 -0600

    visable minifier

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    cssCodeSplit: true, // Optional, if you want to split CSS into separate files
+    minify: false // Disable minification
+  }
 })
